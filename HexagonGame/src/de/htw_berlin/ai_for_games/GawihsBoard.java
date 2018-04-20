866a62c5bb1180ac22ea18b4079921a8e1798c86
Patrick Fehling <19287010+pa-ling@users.noreply.github.com>
2018-04-20 16:38:53 +0200

    Improve board enabling multiple field states per field

--- HexagonGame/src/de/htw_berlin/ai_for_games/GawihsBoard.java
+++ HexagonGame/src/de/htw_berlin/ai_for_games/GawihsBoard.java
@@ -1,60 +1,76 @@
 package de.htw_berlin.ai_for_games;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class GawihsBoard {
 	
 	private enum FieldState {
-		PLAYER_1, PLAYER_2, PLAYER_3, EMPTINESS, DESTROYED
+		PLAYER_1, PLAYER_2, PLAYER_3, UNOCCUPIED, DESTROYED
 	}
 	
 	private final static int SIZE = 9;
 	
-	private FieldState[][] board;
+	private List<Stack<FieldState>> board;
 	
-	public GawihsBoard() {
-		board = new FieldState[SIZE][SIZE];
+	Stack <FieldState> getField(int i, int j){
+		return board.get(j * SIZE + i); //rowIndex * numberOfColumns + columnIndex
+	}
+	
+	public GawihsBoard() {		
+		board = new ArrayList<>(SIZE * SIZE);
+		for (int i = 0; i < SIZE * SIZE; i++) {
+			board.add(new Stack<>());
+		}
 		
-		//Set everything to EMPTINESS
+		//Set everything to unoccupied
 		for (int i = 0; i < SIZE; i++) {
 			for (int j = 0; j < SIZE; j++) {
-				board[i][j] = FieldState.EMPTINESS;
+				getField(i, j).push(FieldState.UNOCCUPIED);
 			}
 		}
 		
 		//Set players
 		for (int i = 0; i < 5; i++) {
-			board[i][0] = FieldState.PLAYER_1;
-			board[i][i+4] = FieldState.PLAYER_2;
-			board[8][8-i] = FieldState.PLAYER_3;
+			getField(i, 0).push(FieldState.PLAYER_1);
+			getField(i, i+4).push(FieldState.PLAYER_2);
+			getField(8, 8-i).push(FieldState.PLAYER_3);
 		}
 		
 		//Set destroyed areas
 		for (int i = 0; i < 4; i++) {
 			for (int j = i; j < 4; j++) {
-				board[j+5][i] = FieldState.DESTROYED;
-				board[i][j+5] = FieldState.DESTROYED;
+				getField(j+5, i).push(FieldState.DESTROYED);
+				getField(i, j+5).push(FieldState.DESTROYED);
 			}
 		}
-		board[4][4] = FieldState.DESTROYED;
+		
+		getField(4, 4).push(FieldState.DESTROYED);		
 	}
 	
 	public void move(int x1, int y1, int x2, int y2) {
-		if (board[x1][y1] == FieldState.EMPTINESS || board[x1][y1] == FieldState.DESTROYED) {
+		Stack<FieldState> sourceField = getField(x1, y1);
+		if (sourceField.peek() == FieldState.UNOCCUPIED || sourceField.peek() == FieldState.DESTROYED) {
 			throw new IllegalStateException("Hey das geht doch nicht! >:-(");
 		}
 
-		if (board[x2][y2] == FieldState.DESTROYED) {
+		Stack<FieldState> targetField = getField(x2, y2);
+		if (targetField.peek() == FieldState.DESTROYED) {
 			throw new IllegalStateException("Hey das geht doch nicht! >:-(");
 		}
 		
-		board[x2][y2] = board[x1][y1];
-		board[x1][y1] = FieldState.DESTROYED;
-		
+		targetField.push(sourceField.pop());
+		if (sourceField.peek() == FieldState.UNOCCUPIED) {
+			sourceField.pop();
+			sourceField.push(FieldState.DESTROYED);
+		}		
 	}
 	
 	public void printBoard() {	
 		for (int j = SIZE - 1; j >= 0; j--) {
 			for (int i = 0; i < SIZE; i++) {
-				System.out.print(board[i][j] + "\t");
+				System.out.print(getField(i, j).peek().toString() + "\t");
 			}
 			System.out.println("");
 		}
