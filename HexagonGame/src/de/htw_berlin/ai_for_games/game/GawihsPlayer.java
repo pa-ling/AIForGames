dc91a5c84f4e98500dd30a87ddf657b51d194965
OHDMax <max@ugschulze.de>
2018-05-01 16:01:36 +0200

    Fix bug which prevented a player from doing any moves
    The bug was caused by a Exception which was thrown as soon as the
    "playerPositions" list was updated. Since the list was initialized using
    the "Arrays.asList()" method, calling "add" and "remove" method on the
    list throws an Exception.

--- HexagonGame/src/de/htw_berlin/ai_for_games/game/GawihsPlayer.java
+++ HexagonGame/src/de/htw_berlin/ai_for_games/game/GawihsPlayer.java
@@ -1,10 +1,11 @@
 package de.htw_berlin.ai_for_games.game;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import de.htw_berlin.ai_for_games.game.GawihsBoard.FieldState;
 import de.htw_berlin.ai_for_games.util.BoardUtil;
@@ -12,72 +13,70 @@ import lenz.htw.gawihs.Move;
 
 public class GawihsPlayer {
 
-    private static List<Field> getStartingPositions(FieldState playerNumber) {
-        switch (playerNumber) {
+    private static List<Field> getStartingPositions(FieldState player) {
+        Stream<Field> stream;
+
+        switch (player) {
             case PLAYER_0:
-                return Arrays.asList(new Field(0, 0), new Field(1, 0), new Field(2, 0), new Field(3, 0),
-                        new Field(4, 0));
+                stream = Stream.of(new Field(0, 0), new Field(1, 0), new Field(2, 0), new Field(3, 0), new Field(4, 0));
+                break;
             case PLAYER_1:
-                return Arrays.asList(new Field(0, 4), new Field(1, 5), new Field(2, 6), new Field(3, 7),
-                        new Field(4, 8));
+                stream = Stream.of(new Field(0, 4), new Field(1, 5), new Field(2, 6), new Field(3, 7), new Field(4, 8));
+                break;
             case PLAYER_2:
-                return Arrays.asList(new Field(8, 4), new Field(8, 5), new Field(8, 6), new Field(8, 7),
-                        new Field(8, 8));
+                stream = Stream.of(new Field(8, 4), new Field(8, 5), new Field(8, 6), new Field(8, 7), new Field(8, 8));
             default:
                 return new ArrayList<>();
         }
+
+        return stream.collect(Collectors.toCollection(ArrayList::new));
     }
 
     private final FieldState playerNumber;
 
-    private final List<Field> playerPositions;
+    private final List<Field> playerStonePositions;
 
     private final GawihsBoard board;
 
     public GawihsPlayer(int playerNumber, GawihsBoard board) {
         this.playerNumber = FieldState.values()[playerNumber];
         this.board = board;
-        this.playerPositions = getStartingPositions(this.playerNumber);
+        this.playerStonePositions = getStartingPositions(this.playerNumber);
     }
 
-    private Set<Move> getPossibleMoves() {
-        Set<Move> validMoves = new HashSet<>();
-        for (Field playerPosition : this.playerPositions) { // TODO: remove blocked stones
-            for (Field targetField : getTargetFields()) {
-                for (Field field : BoardUtil.getFieldsAround(targetField.x, targetField.y)) {
-                    // ist an dieser Stelle ein andere Stein, als der aktuelle Stein?
-                    if (!field.equals(playerPosition) && this.playerPositions.contains(field)) {
-                        validMoves.add(new Move(playerPosition.x, playerPosition.y, targetField.x, targetField.y));
+    private List<Move> getPossibleMoves() {
+        Set<Field> targetFields = new HashSet<>();
+
+        // get possible target fields
+        for (Field currentPlayerPosition : this.playerStonePositions) {
+            targetFields.addAll(BoardUtil.getAvailableFieldsForPlayerAround(currentPlayerPosition.x,
+                    currentPlayerPosition.y, this.board, this.playerNumber));
+        }
+
+        // compute possible moves
+        // ein Zug ist möglich, wenn um das Zielfeld mindestens ein Spielerstein liegt
+        // und dieser Stein nicht der Stein ist, den wir gerade bewegen wollen
+        List<Move> possibleMoves = new ArrayList<>();
+        for (Field stoneToMove : this.playerStonePositions) {
+            // TODO: Don't move blocked stones
+            for (Field targetField : targetFields) {
+                for (Field fieldAroundTarget : BoardUtil.getFieldsAround(targetField.x, targetField.y)) {
+                    if (this.playerStonePositions.contains(fieldAroundTarget) && !stoneToMove.equals(fieldAroundTarget)) {
+                        possibleMoves.add(new Move(stoneToMove.x, stoneToMove.y, targetField.x, targetField.y));
                     }
                 }
             }
         }
-        return validMoves;
-    }
 
-    private Set<Field> getTargetFields() {
-        Set<Field> fields = new HashSet<>();
-
-        for (Field currentPlayerPosition : this.playerPositions) {
-            fields.addAll(BoardUtil.getAvailableFieldsForPlayerAround(currentPlayerPosition.x, currentPlayerPosition.y, this.board, this.playerNumber));
-        }
-
-        // System.out.println("validTargetFields:");
-        // for (Field field : fields) {
-        // System.out.println(
-        // "(" + field.x + "," + field.y + "):" + this.board.getField(field.x,
-        // field.y).peek().toString());
-        // }
-
-        return fields;
+        return possibleMoves;
     }
 
     public Move move() {
         Move moveToSend = getPossibleMoves().stream().findFirst().get();
 
         // update playerPositions
-        this.playerPositions.remove(new Field(moveToSend.fromX, moveToSend.fromY));
-        this.playerPositions.add(new Field(moveToSend.toX, moveToSend.toY));
+        this.playerStonePositions.remove(new Field(moveToSend.fromX, moveToSend.fromY));
+        this.playerStonePositions.add(new Field(moveToSend.toX, moveToSend.toY));
 
         return moveToSend;
     }
