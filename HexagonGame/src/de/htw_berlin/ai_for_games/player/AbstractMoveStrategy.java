c66daa0b7e770356cc5c4480f1eb3870c46272d8
Patrick Fehling <19287010+pa-ling@users.noreply.github.com>
2018-05-21 16:53:26 +0200

    Move getPossibleMoves method to abstract strategy

--- HexagonGame/src/de/htw_berlin/ai_for_games/player/AbstractMoveStrategy.java
+++ HexagonGame/src/de/htw_berlin/ai_for_games/player/AbstractMoveStrategy.java
@@ -1,11 +1,49 @@
 package de.htw_berlin.ai_for_games.player;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import de.htw_berlin.ai_for_games.board.Field;
 import de.htw_berlin.ai_for_games.board.GawihsBoard;
+import lenz.htw.gawihs.Move;
 
 public abstract class AbstractMoveStrategy implements MoveStrategy {
 
     protected GawihsBoard board;
     protected GawihsPlayer player;
+
+    @Override
+    public List<Move> getPossibleMoves() {
+        Set<Field> targetFields = new HashSet<>();
+
+        List<Field> availablePlayerStones = new ArrayList<>();
+
+        // get possible target fields and remove unavailable player stones
+        for (Field playerStone : this.player.getPlayerStonePositions()) {
+            if (!this.board.isPlayerOnTopOfField(playerStone, this.player)) {
+                continue;
+            }
+            availablePlayerStones.add(playerStone);
+            targetFields.addAll(this.board.getAvailableFieldsForPlayerAround(playerStone, this.player));
+        }
+
+        // compute possible moves
+        List<Move> possibleMoves = new ArrayList<>();
+        for (Field stoneToMove : availablePlayerStones) {
+            for (Field targetField : targetFields) {
+                for (Field fieldAroundTarget : GawihsBoard.getFieldsAround(targetField)) {
+                    if (this.player.getPlayerStonePositions().contains(fieldAroundTarget)
+                            && !stoneToMove.equals(fieldAroundTarget)) {
+                        possibleMoves.add(new Move(stoneToMove.x, stoneToMove.y, targetField.x, targetField.y));
+                    }
+                }
+            }
+        }
+
+        return possibleMoves;
+    }
 
     @Override
     public void setBoard(GawihsBoard board) {
--- HexagonGame/src/de/htw_berlin/ai_for_games/player/RandomMoveStrategy.java
+++ HexagonGame/src/de/htw_berlin/ai_for_games/player/RandomMoveStrategy.java
@@ -1,13 +1,7 @@
 package de.htw_berlin.ai_for_games.player;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 
-import de.htw_berlin.ai_for_games.board.Field;
-import de.htw_berlin.ai_for_games.board.GawihsBoard;
 import lenz.htw.gawihs.Move;
 
 /**
@@ -24,37 +18,6 @@ public class RandomMoveStrategy extends AbstractMoveStrategy {
             return null;
         }
         return getPossibleMoves().get(ThreadLocalRandom.current().nextInt(size));
-    }
-
-    @Override
-    public List<Move> getPossibleMoves() {
-        Set<Field> targetFields = new HashSet<>();
-
-        List<Field> availablePlayerStones = new ArrayList<>();
-
-        // get possible target fields and remove unavailable player stones
-        for (Field playerStone : this.player.getPlayerStonePositions()) {
-            if (!this.board.isPlayerOnTopOfField(playerStone, this.player)) {
-                continue;
-            }
-            availablePlayerStones.add(playerStone);
-            targetFields.addAll(this.board.getAvailableFieldsForPlayerAround(playerStone, this.player));
-        }
-
-        // compute possible moves
-        List<Move> possibleMoves = new ArrayList<>();
-        for (Field stoneToMove : availablePlayerStones) {
-            for (Field targetField : targetFields) {
-                for (Field fieldAroundTarget : GawihsBoard.getFieldsAround(targetField)) {
-                    if (this.player.getPlayerStonePositions().contains(fieldAroundTarget)
-                            && !stoneToMove.equals(fieldAroundTarget)) {
-                        possibleMoves.add(new Move(stoneToMove.x, stoneToMove.y, targetField.x, targetField.y));
-                    }
-                }
-            }
-        }
-
-        return possibleMoves;
     }
 
 }
