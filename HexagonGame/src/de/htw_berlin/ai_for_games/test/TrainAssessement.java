3a2e95de6157f467c82d6a894c3fe18f5d2835a9
Patrick Fehling <19287010+pa-ling@users.noreply.github.com>
2018-05-25 19:58:31 +0200

    Add method for creating and printing candidates

--- HexagonGame/src/de/htw_berlin/ai_for_games/test/TrainAssessement.java
+++ HexagonGame/src/de/htw_berlin/ai_for_games/test/TrainAssessement.java
@@ -4,11 +4,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
@@ -38,6 +40,20 @@ public class TrainAssessement {
         }
     }
 
+    private static List<AssessmentConfig> createCandidates(int count) {
+        List<AssessmentConfig> candidates = new ArrayList<>();
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        for (int i = 0; i < count; i++) {
+            // TODO: maybe check for linear independance among the candidates;
+            AssessmentConfig config = new AssessmentConfig(random.nextInt(1, Integer.MAX_VALUE),
+                    random.nextInt(1, Integer.MAX_VALUE), random.nextInt(1, Integer.MAX_VALUE),
+                    random.nextInt(1, Integer.MAX_VALUE), random.nextInt(1, Integer.MAX_VALUE),
+                    random.nextInt(1, Integer.MAX_VALUE));
+            candidates.add(config);
+        }
+        return candidates;
+    }
+
     private static void writeConfig(AssessmentConfig config, String filePath) throws FileNotFoundException {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String jsonContent = gson.toJson(config, AssessmentConfig.class);
@@ -53,9 +69,7 @@ public class TrainAssessement {
     private final boolean AUTOMATIC_GAME_SERVER_STARTUP = true;
     private final String LOGO_PATH = "res/claptrap.png";
 
-    private List<AssessmentConfig> createCandidates() {
-        return null;
-    }
+    private final int CANDIDATES_COUNT = 21;
 
     private HashMap<AssessmentConfig, Integer> evaluateCandidates(List<AssessmentConfig> candidates) {
         return null;
@@ -63,6 +77,12 @@ public class TrainAssessement {
 
     private List<AssessmentConfig> mutateCandidates(List<AssessmentConfig> candidates) {
         return null;
+    }
+
+    private void printCandidates(List<AssessmentConfig> candidates) {
+        for (AssessmentConfig config : candidates) {
+            System.out.println(config);
+        }
     }
 
     private List<AssessmentConfig> recombineCandidates(List<AssessmentConfig> candidates) {
