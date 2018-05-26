964535eecc97ff02e0a301bd5d3171c235ccbfe6
Patrick Fehling <19287010+pa-ling@users.noreply.github.com>
2018-05-26 10:58:38 +0200

    Add functionality to get the score of a game from the server

--- HexagonGame/src/de/htw_berlin/ai_for_games/test/ExecuteAssessmentTraining.java
+++ HexagonGame/src/de/htw_berlin/ai_for_games/test/ExecuteAssessmentTraining.java
@@ -1,49 +1,63 @@
 package de.htw_berlin.ai_for_games.test;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.util.Optional;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 class ExecuteAssessmentTraining {
 
-    private final static String[] NAMES = { "Alice", "Bob", "Carol" };
+    private final static String[] NAMES = { "ALICE", "BOB", "CAROL" };
     private final static String[] CONFIGS = { "res/configA.json", "res/configB.json", "res/configC.json" };
-    private final static boolean AUTOMATIC_GAME_SERVER_STARTUP = true;
     private final static String LOGO_PATH = "res/claptrap.png";
 
     private final static int CANDIDATES_COUNT = 21;
 
     public static void main(String[] args) throws InterruptedException, IOException {
+        System.out.println(playAGame(NAMES, CONFIGS, LOGO_PATH));
+    }
+
+    private static Map<String, Long> playAGame(String[] names, String[] configs, String logoPath)
+            throws IOException, InterruptedException {
         // start server
-        Optional<Process> process = Optional.empty();
-        if (AUTOMATIC_GAME_SERVER_STARTUP) {
-            process = Optional
-                    .of(new ProcessBuilder("java", "-Djava.library.path=lib/native", "-jar", "gawihs.jar", "800", "600",
-                            "5", "showcoords", "noanim", "autoclose").directory(new File("lib")).inheritIO().start());
-            Thread.sleep(1000);
-        }
+        ProcessBuilder processBuilder = new ProcessBuilder("java", "-Djava.library.path=lib/native", "-jar",
+                "gawihs.jar", "800", "600", "5", "noanim", "autoclose").directory(new File("lib"));
+        Process process = processBuilder.start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        Thread.sleep(1000);
 
         // start Clients
-        ExecutorService executor = Executors.newFixedThreadPool(NAMES.length);
-        for (int i = 0; i < NAMES.length; i++) {
-            String name = NAMES[i];
-            String config = CONFIGS[i];
-            GameClientTask gameClientTask = new GameClientTask("127.0.0.1", name, LOGO_PATH, config);
+        ExecutorService executor = Executors.newFixedThreadPool(names.length);
+        for (int i = 0; i < names.length; i++) {
+            String name = names[i];
+            String config = configs[i];
+            GameClientTask gameClientTask = new GameClientTask("127.0.0.1", name, logoPath, config);
             executor.execute(gameClientTask);
         }
         executor.shutdown();
         executor.awaitTermination(1, TimeUnit.HOURS);
 
-        process.ifPresent(p -> {
-            try {
-                p.waitFor();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        // get Scores
+        String line = null;
+        Map<String, Long> scores = new HashMap<>(3);
+        while ((line = reader.readLine()) != null) {
+            if (!line.startsWith("Final result:")) {
+                continue;
             }
-        });
+
+            for (int i = 0; i < 3; i++) {
+                String[] placementString = reader.readLine().split(": ");
+                scores.put(placementString[0], Long.valueOf(placementString[1]));
+            }
+            break;
+        }
+
+        return scores;
     }
 
 }
