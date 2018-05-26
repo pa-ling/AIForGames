d9be260764ddc898f15299a114d7006d907da468
Patrick Fehling <19287010+pa-ling@users.noreply.github.com>
2018-05-26 15:06:26 +0200

    Update GawihsClientTest

--- HexagonGame/src/de/htw_berlin/ai_for_games/test/GawihsClientTest.java
+++ HexagonGame/src/de/htw_berlin/ai_for_games/test/GawihsClientTest.java
@@ -1,65 +1,22 @@
 package de.htw_berlin.ai_for_games.test;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Optional;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.Map;
 
 import org.junit.Test;
 
-import de.htw_berlin.ai_for_games.GawihsClient;
-
 public class GawihsClientTest {
 
-    private class GameClientTask implements Runnable {
-        private final String host, name, logoPath;
-
-        public GameClientTask(String host, String name, String logoPath) {
-            this.host = host;
-            this.name = name;
-            this.logoPath = logoPath;
-        }
-
-        @Override
-        public void run() {
-            String[] args = { this.host, this.name, this.logoPath };
-            GawihsClient.main(args);
-        }
-    }
-
-    private final String[] NAMES = { "Alice", "Bob", "Carol" };
-    private final boolean AUTOMATIC_GAME_SERVER_STARTUP = true;
-
+    private final String[] CONFIGS = { "res/configA.json", "res/configB.json", "res/configC.json" };
+    // private final static String[] CONFIGS = { "res/config.json",
+    // "res/config.json", "res/config.json" };
+    private final String[] NAMES = { "ALICE", "BOB", "CAROL" };
     private final String LOGO_PATH = "res/claptrap.png";
 
     @Test
     public void testGame() throws InterruptedException, IOException {
-        // start server
-        Optional<Process> process = Optional.empty();
-        if (this.AUTOMATIC_GAME_SERVER_STARTUP) {
-            process = Optional.of(new ProcessBuilder("java", "-Djava.library.path=lib/native", "-jar", "gawihs.jar",
-                    "800", "600", "5", "showcoords").directory(new File("lib")).inheritIO().start());
-            Thread.sleep(1000);
-        }
-
-        // start Clients
-        ExecutorService executor = Executors.newFixedThreadPool(this.NAMES.length);
-        for (final String name : this.NAMES) {
-            GameClientTask gameClientTask = new GameClientTask("127.0.0.1", name, this.LOGO_PATH);
-            executor.execute(gameClientTask);
-        }
-        executor.shutdown();
-        executor.awaitTermination(1, TimeUnit.HOURS);
-
-        process.ifPresent(p -> {
-            try {
-                p.waitFor();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+        Map<String, Integer> scores = AssessmentTraining.playAGame(this.NAMES, this.CONFIGS, this.LOGO_PATH);
+        System.out.println("Score: " + scores);
     }
 
 }
