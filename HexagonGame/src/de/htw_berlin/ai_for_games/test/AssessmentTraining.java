fd373cb28213d3bba209a73d59105af6e5f719c5
Patrick Fehling <19287010+pa-ling@users.noreply.github.com>
2018-05-26 11:11:31 +0200

    Change visibility of AssessmentTraining methods and move playAGame()

--- HexagonGame/src/de/htw_berlin/ai_for_games/test/AssessmentTraining.java
+++ HexagonGame/src/de/htw_berlin/ai_for_games/test/AssessmentTraining.java
@@ -1,11 +1,19 @@
 package de.htw_berlin.ai_for_games.test;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -14,7 +22,7 @@ import de.htw_berlin.ai_for_games.player.strategies.AssessmentConfig;
 
 class AssessmentTraining {
 
-    private static List<AssessmentConfig> createCandidates(int count) {
+    public static List<AssessmentConfig> createCandidates(int count) {
         List<AssessmentConfig> candidates = new ArrayList<>();
         ThreadLocalRandom random = ThreadLocalRandom.current();
         for (int i = 0; i < count; i++) {
@@ -28,6 +36,44 @@ class AssessmentTraining {
         return candidates;
     }
 
+    public static Map<String, Long> playAGame(String[] names, String[] configs, String logoPath)
+            throws IOException, InterruptedException {
+        // start server
+        ProcessBuilder processBuilder = new ProcessBuilder("java", "-Djava.library.path=lib/native", "-jar",
+                "gawihs.jar", "800", "600", "5", "noanim", "autoclose").directory(new File("lib"));
+        Process process = processBuilder.start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        Thread.sleep(1000);
+
+        // start Clients
+        ExecutorService executor = Executors.newFixedThreadPool(names.length);
+        for (int i = 0; i < names.length; i++) {
+            String name = names[i];
+            String config = configs[i];
+            GameClientTask gameClientTask = new GameClientTask("127.0.0.1", name, logoPath, config);
+            executor.execute(gameClientTask);
+        }
+        executor.shutdown();
+        executor.awaitTermination(1, TimeUnit.HOURS);
+
+        // get Scores
+        String line = null;
+        Map<String, Long> scores = new HashMap<>(3);
+        while ((line = reader.readLine()) != null) {
+            if (!line.startsWith("Final result:")) {
+                continue;
+            }
+
+            for (int i = 0; i < 3; i++) {
+                String[] placementString = reader.readLine().split(": ");
+                scores.put(placementString[0], Long.valueOf(placementString[1]));
+            }
+            break;
+        }
+
+        return scores;
+    }
+
     private static void writeConfig(AssessmentConfig config, String filePath) throws FileNotFoundException {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String jsonContent = gson.toJson(config, AssessmentConfig.class);
@@ -38,11 +84,11 @@ class AssessmentTraining {
 
     }
 
-    private HashMap<AssessmentConfig, Integer> evaluateCandidates(List<AssessmentConfig> candidates) {
+    public HashMap<AssessmentConfig, Integer> evaluateCandidates(List<AssessmentConfig> candidates) {
         return null;
     }
 
-    private List<AssessmentConfig> mutateCandidates(List<AssessmentConfig> candidates) {
+    public List<AssessmentConfig> mutateCandidates(List<AssessmentConfig> candidates) {
         return null;
     }
 
@@ -52,11 +98,11 @@ class AssessmentTraining {
         }
     }
 
-    private List<AssessmentConfig> recombineCandidates(List<AssessmentConfig> candidates) {
+    public List<AssessmentConfig> recombineCandidates(List<AssessmentConfig> candidates) {
         return null;
     }
 
-    private List<AssessmentConfig> selectCandidates(HashMap<AssessmentConfig, Integer> candidates) {
+    public List<AssessmentConfig> selectCandidates(HashMap<AssessmentConfig, Integer> candidates) {
         return null;
     }
 
--- HexagonGame/src/de/htw_berlin/ai_for_games/test/ExecuteAssessmentTraining.java
+++ HexagonGame/src/de/htw_berlin/ai_for_games/test/ExecuteAssessmentTraining.java
@@ -1,14 +1,6 @@
 package de.htw_berlin.ai_for_games.test;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 class ExecuteAssessmentTraining {
 
@@ -19,45 +11,7 @@ class ExecuteAssessmentTraining {
     private final static int CANDIDATES_COUNT = 21;
 
     public static void main(String[] args) throws InterruptedException, IOException {
-        System.out.println(playAGame(NAMES, CONFIGS, LOGO_PATH));
-    }
-
-    private static Map<String, Long> playAGame(String[] names, String[] configs, String logoPath)
-            throws IOException, InterruptedException {
-        // start server
-        ProcessBuilder processBuilder = new ProcessBuilder("java", "-Djava.library.path=lib/native", "-jar",
-                "gawihs.jar", "800", "600", "5", "noanim", "autoclose").directory(new File("lib"));
-        Process process = processBuilder.start();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        Thread.sleep(1000);
-
-        // start Clients
-        ExecutorService executor = Executors.newFixedThreadPool(names.length);
-        for (int i = 0; i < names.length; i++) {
-            String name = names[i];
-            String config = configs[i];
-            GameClientTask gameClientTask = new GameClientTask("127.0.0.1", name, logoPath, config);
-            executor.execute(gameClientTask);
-        }
-        executor.shutdown();
-        executor.awaitTermination(1, TimeUnit.HOURS);
-
-        // get Scores
-        String line = null;
-        Map<String, Long> scores = new HashMap<>(3);
-        while ((line = reader.readLine()) != null) {
-            if (!line.startsWith("Final result:")) {
-                continue;
-            }
-
-            for (int i = 0; i < 3; i++) {
-                String[] placementString = reader.readLine().split(": ");
-                scores.put(placementString[0], Long.valueOf(placementString[1]));
-            }
-            break;
-        }
-
-        return scores;
+        System.out.println(AssessmentTraining.playAGame(NAMES, CONFIGS, LOGO_PATH));
     }
 
 }
