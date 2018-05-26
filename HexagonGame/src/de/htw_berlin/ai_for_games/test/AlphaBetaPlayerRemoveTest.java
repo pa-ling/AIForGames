d79529b17c50816139e0ec0b1dccf4877b5edd64
OHDMax <max@ugschulze.de>
2018-05-26 14:14:55 +0200

    Add test to check for correct player removal

---
+++ HexagonGame/src/de/htw_berlin/ai_for_games/test/AlphaBetaPlayerRemoveTest.java
@@ -0,0 +1,41 @@
+package de.htw_berlin.ai_for_games.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import de.htw_berlin.ai_for_games.board.GawihsBoard;
+import de.htw_berlin.ai_for_games.player.GawihsAIPlayer;
+import de.htw_berlin.ai_for_games.player.GawihsPlayer;
+import de.htw_berlin.ai_for_games.player.strategies.AlphaBetaPruningStrategy;
+
+class AlphaBetaPlayerRemoveTest {
+
+    static GawihsBoard board;
+    static GawihsAIPlayer player1;
+    static GawihsPlayer player2;
+    static GawihsPlayer player3;
+
+    @BeforeAll
+    static void setup() {
+        board = new GawihsBoard();
+        player1 = new GawihsAIPlayer(0, new AlphaBetaPruningStrategy("HexagonGame/res/configP.json"), board);
+        player2 = new GawihsPlayer(1, board);
+        player3 = new GawihsPlayer(2, board);
+        List<GawihsPlayer> enemies = new ArrayList<>();
+        enemies.add(player2);
+        enemies.add(player3);
+        player1.setEnemies(enemies);
+    }
+
+    @Test
+    void test() {
+        player1.move();
+        player1.removeEnemy(player2);
+        player1.move();
+
+    }
+
+}
