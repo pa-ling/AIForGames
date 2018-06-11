0a65cc122c0f34236cc21017c2f75fce55e38f53
Patrick Fehling <19287010+pa-ling@users.noreply.github.com>
2018-06-11 17:48:58 +0200

    Fix test game start

--- PaintGame/src/de/htw_berlin/ai_for_games/test/ZpifubClientTest.java
+++ PaintGame/src/de/htw_berlin/ai_for_games/test/ZpifubClientTest.java
@@ -13,19 +13,17 @@ public class ZpifubClientTest {
     private final String[] NAMES = { "ALICE", "BOB", "CAROL" };
     private final String[] MESSAGES = { "nice!", "good!", "awesome!" };
     private final String HOST_IP = "127.0.0.1";
+    private final int SLEEP_TIME = 4000;
 
     @Test
     public void testGame() throws InterruptedException, IOException {
         playAGame(NAMES, MESSAGES, HOST_IP);
     }
 
-    public static void playAGame(String[] names, String[] messages, String host)
-            throws IOException, InterruptedException {
+    public void playAGame(String[] names, String[] messages, String host) throws IOException, InterruptedException {
         // start server
-        ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", "zpifub.jar").inheritIO()
-                .directory(new File("lib"));
-        Process process = processBuilder.start();
-        Thread.sleep(1000);
+        new ProcessBuilder("java", "-jar", "zpifub.jar").inheritIO().directory(new File("lib")).start();
+        Thread.sleep(SLEEP_TIME);
 
         // start Clients
         ExecutorService executor = Executors.newFixedThreadPool(names.length);
