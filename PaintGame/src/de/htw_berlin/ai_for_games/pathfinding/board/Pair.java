5fdd1e7ac4f0bc4f42d354781db174c5e8505380
OHDMax <max@ugschulze.de>
2018-07-01 23:40:18 +0200

    Implement equals method in pair

--- PaintGame/src/de/htw_berlin/ai_for_games/pathfinding/board/Pair.java
+++ PaintGame/src/de/htw_berlin/ai_for_games/pathfinding/board/Pair.java
@@ -10,4 +10,39 @@ public class Pair {
         this.y = y;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Pair)) {
+            return false;
+        }
+        Pair other = (Pair) obj;
+        if (this.x != other.x) {
+            return false;
+        }
+        if (this.y != other.y) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + this.x;
+        result = prime * result + this.y;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair [x=" + this.x + ", y=" + this.y + "]";
+    }
+
 }
