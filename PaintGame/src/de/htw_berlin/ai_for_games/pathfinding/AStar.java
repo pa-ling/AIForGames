96be33f7b20ca5b39a0d7eccd8de44ae82cd8c9d
OHDMax <max@ugschulze.de>
2018-07-02 16:09:14 +0200

    Change AStar to new implementation

--- PaintGame/src/de/htw_berlin/ai_for_games/pathfinding/AStar.java
+++ PaintGame/src/de/htw_berlin/ai_for_games/pathfinding/AStar.java
@@ -12,47 +12,63 @@ import de.htw_berlin.ai_for_games.pathfinding.board.Pair;
 
 public class AStar {
 
-    private Set<Pair> closedSet = new HashSet<>();
-    private Set<Pair> openSet = new HashSet<>();
-    private Map<Pair, Pair> cameFrom = new HashMap<>();
-    private Map<Pair, Integer> gScore = new HashMap<>();
-    private Map<Pair, Integer> fScore = new HashMap<>();
+    private static int calculateHeuristic(final Pair node, final Pair targetNode) {
+        // direct distance between node and targetNode
+        return Math.abs(targetNode.x - node.x) + Math.abs(targetNode.y - node.y);
+    }
+
+    private final Set<Pair> closedSet = new HashSet<>();
+    private final Set<Pair> openSet = new HashSet<>();
+    private final Map<Pair, Pair> cameFrom = new HashMap<>();
+    private final Map<Pair, Integer> gScore = new HashMap<>();
+    private final Map<Pair, Integer> fScore = new HashMap<>();
+
+    private Pair getNodeWithLowestFScore() {
+        Pair currentNodeWithLowestFScore = null;
+        int currentLowestFScore = Integer.MAX_VALUE;
+        for (Pair node : this.openSet) {
+            int currentFScore = this.fScore.get(node);
+            if (currentFScore < currentLowestFScore) {
+                currentNodeWithLowestFScore = node;
+                currentLowestFScore = currentFScore;
+            }
+        }
+        return currentNodeWithLowestFScore;
+    }
 
     public List<Pair> getPath(Layer pathLayer, Pair start, Pair target) {
-        openSet.add(start);
-        gScore.put(start, 0);
-        fScore.put(start, calculateHeuristic(start, target));
+        this.openSet.add(start);
+        this.gScore.put(start, 0);
+        this.fScore.put(start, calculateHeuristic(start, target));
 
-        while (!openSet.isEmpty()) {
+        while (!this.openSet.isEmpty()) {
             Pair current = getNodeWithLowestFScore();
-
             if (current.equals(target)) {
                 return reconstructPath(current);
             }
 
-            openSet.remove(current);
-
-            closedSet.add(current);
+            this.openSet.remove(current);
+            this.closedSet.add(current);
 
             List<Pair> neighbours = pathLayer.getNeighbors(current.x, current.y);
             for (Pair neighbour : neighbours) {
-                if (closedSet.contains(neighbour)) {
+                if (this.closedSet.contains(neighbour)) {
                     continue;
                 }
 
-                if (openSet.contains(neighbour)) {
-                    openSet.add(neighbour);
+                if (!this.openSet.contains(neighbour)) {
+                    this.openSet.add(neighbour);
                 }
 
-                int tentativeGScore = gScore.get(current) + pathLayer.getCost(neighbour.x, neighbour.y);
-                Integer currentGScore = gScore.get(neighbour);
+                int tentativeGScore = this.gScore.get(current) + pathLayer.getCost(neighbour.x, neighbour.y);
+                Integer currentGScore = this.gScore.get(neighbour);
                 if (currentGScore != null && tentativeGScore >= currentGScore) {
                     continue;
                 }
 
-                cameFrom.put(neighbour, current);
-                gScore.put(neighbour, tentativeGScore);
-                fScore.put(neighbour, tentativeGScore + calculateHeuristic(neighbour, target));
+                this.cameFrom.put(neighbour, current);
+                this.gScore.put(neighbour, tentativeGScore);
+                this.fScore.put(neighbour, tentativeGScore + calculateHeuristic(neighbour, target));
             }
 
         }
@@ -65,29 +81,11 @@ public class AStar {
         List<Pair> totalPath = new ArrayList<>();
         totalPath.add(current);
 
-        while (cameFrom.containsKey(current)) {
-            current = cameFrom.get(current);
+        while (this.cameFrom.containsKey(current)) {
+            current = this.cameFrom.get(current);
             totalPath.add(0, current); // prepend
         }
         return totalPath;
-    }
-
-    private Pair getNodeWithLowestFScore() {
-        Pair currentNodeWithLowestFScore = null;
-        int currentLowestFScore = Integer.MAX_VALUE;
-        for (Pair node : openSet) {
-            int currentFScore = this.fScore.get(node);
-            if (currentFScore < currentLowestFScore) {
-                currentNodeWithLowestFScore = node;
-                currentLowestFScore = currentFScore;
-            }
-        }
-        return currentNodeWithLowestFScore;
-    }
-
-    private static int calculateHeuristic(final Pair node, final Pair targetNode) {
-        // direct distance between node and targetNode
-        return Math.abs(targetNode.x - node.x) + Math.abs(targetNode.y - node.y);
     }
 
 }
