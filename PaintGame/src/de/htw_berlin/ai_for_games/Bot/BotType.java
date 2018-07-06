ef992f15a11de3f0f0e60cc0a59eeaa89dfd3908
Patrick Fehling <19287010+pa-ling@users.noreply.github.com>
2018-07-06 20:20:27 +0200

    Add speed for each bot

--- PaintGame/src/de/htw_berlin/ai_for_games/Bot/BotType.java
+++ PaintGame/src/de/htw_berlin/ai_for_games/Bot/BotType.java
@@ -2,16 +2,18 @@ package de.htw_berlin.ai_for_games.Bot;
 
 public enum BotType {
 
-    SPRAY_CAN(0),
+    SPRAY_CAN(0, 2.1f),
 
-    SMALL_BRUSH(1),
+    SMALL_BRUSH(1, 1.0f),
 
-    LARGE_BRUSH(2);
+    LARGE_BRUSH(2, 0.68f);
 
     public final int number;
+    public final float speed;
 
-    private BotType(int number) {
+    private BotType(int number, float speed) {
         this.number = number;
+        this.speed = speed;
     }
 
 }
